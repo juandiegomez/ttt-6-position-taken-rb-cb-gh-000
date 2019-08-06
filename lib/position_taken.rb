9566ec12ac79b302ad9c06ac7a69e444ef6b421a
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-06 03:05:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
  def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
-  else 
+  else
     return false
   end
 end
